88ae9c8a1ff171d2cc1687754229220c043b94c9
Tomaz Golob <tomaz.golob@mediately.co>
2026-01-09 21:37:41 +0100

    Initial commit

---
+++ src/main/java/com/prpo/entry/PrpoEntryApplication.java
@@ -0,0 +1,13 @@
+package com.prpo.entry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrpoEntryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrpoEntryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prpo/entry/PrpoEntryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prpo.entry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrpoEntryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
